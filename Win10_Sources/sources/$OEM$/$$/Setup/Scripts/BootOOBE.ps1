1a44fe66310f32d1088e3ca577ef7518a1f0a1fa
Roger Zander <roger@zander.ch>
2020-07-10 11:12:18 +0200

    Update BootOOBE.ps1

--- Win10_Sources/sources/$OEM$/$$/Setup/Scripts/BootOOBE.ps1
+++ Win10_Sources/sources/$OEM$/$$/Setup/Scripts/BootOOBE.ps1
@@ -8,5 +8,7 @@ Rename-Item C:\Windows\Setup\Scripts\init2.ps1 init.ps1 -ea SilentlyContinue #Ru
 
 get-process sysprep -ea SilentlyContinue | stop-process -Force -ErrorAction SilentlyContinue
 Start-Sleep 2
-&C:\Windows\System32\Sysprep\sysprep.exe /oobe /shutdown /unattend:c:\windows\system32\sysprep\unattend.xml
+
+#old &C:\Windows\System32\Sysprep\sysprep.exe /oobe /shutdown /unattend:c:\windows\system32\sysprep\unattend.xml
+Start-Process -FilePath "C:\Windows\System32\Sysprep\sysprep.exe" -ArgumentList "/oobe /quiet /reboot /unattend:C:\Windows\system32\sysprep\unattend.xml" -Wait 
 exit(0)
